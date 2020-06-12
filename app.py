4df41e40c5ef4a232efebb59db1908ed3717a27e
Pedro Eugénio Gomes <p.geno_88@hotmail.com>
2020-06-12 20:44:43 +0100

    First commit, calculate number of stocks needed to achieve a given goal

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        dividendGoal = request.form['dividendGoal']
+        stockSymbol = request.form['stockSymbol']
+        
+        stock = yf.Ticker(stockSymbol)
+        info = stock.info
+        closeMarketValue = info['regularMarketPreviousClose']
+        dividendValue = info['dividendRate']
+        dividendYield = info['dividendYield']
+        dividendYieldPercent = round(dividendValue * 100, 2)
+
+        totalStockNumber = round(float(dividendGoal) / float(dividendValue) * float(closeMarketValue))
+
+        return render_template('app.html', totalStocks=totalStockNumber, goal=dividendGoal)
+
+
+if __name__ == ' __main__':
+    app.debug = True
+    app.run()
